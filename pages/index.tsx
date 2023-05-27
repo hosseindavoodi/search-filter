8c1a2e30afda0938dc4b6e3579795260af18129c
Hossein Davoodi <h.davoodi@trenkwalder.com>
2023-05-27 11:17:39 +0300

    Added server side rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,18 @@
 import { useEffect, useState } from "react"
+import type { InferGetServerSidePropsType, GetServerSideProps } from 'next';
 import Header from "@/components/header"
 import DataRender from "@/components/dataRender"
 
-export default function Home() {
+export default function Home({response}: any) {
 const [data, setData] = useState([])
 const [search, setSearch] = useState('')
 const [isDoctor, setIsDoctor] = useState(false)
 const [isUnder21, setIsUnder21] = useState(false)
 
 
-  const fetchData = async() => {
-    try {
-      const request = await fetch('/userData.json')
-      const response = await request.json()
-      setData(response)
-    }
-    catch {
-      console.log('Something is wrong')
-    }
-  }
-
-  useEffect(()=>{
-    fetchData()
-  },[])
+    useEffect(()=>{
+    setData(response)
+  },[response])
 
   return (
     <div className="p-4">
@@ -42,3 +32,13 @@ const [isUnder21, setIsUnder21] = useState(false)
     </div>
   )
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+
+  const request = await fetch('http://localhost:3000/userData.json');
+  const response = await request.json();
+   
+  return { 
+    props: { response } 
+  };
+};
